d5e20fd7e0f297525a8af6122df5eb1444bc2dc2
Your Name <you@example.com>
2025-06-30 00:59:10 +0530

    homepage react

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <hr className='border-bottom'/>
+      <p className='text-light text-center'>&copy; 2025 - Built with ❤️ by Jayneet Vyas</p>
+    </footer>
+  )
+}
+
+export default Footer
