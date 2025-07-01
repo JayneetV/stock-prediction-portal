9f787d8d4caf1ce507b622206174b265e3211486
Your Name <you@example.com>
2025-07-02 02:10:13 +0530

    registration page

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -3,15 +3,15 @@ import Button from './Button'
 
 const Main = () => {
   return (
-   <>
-    <div className='container'>
+    <>
+      <div className='container'>
         <div className='p-5 text-center bg-light-dark rounded'>
-            <h1 className='text-light'>Stock Prediction Portal</h1>
-            <p className='text-light lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deleniti commodi quasi quis. Sunt, et id libero nam voluptates quam! Distinctio possimus architecto accusantium aspernatur alias cupiditate laborum quia delectus iste explicabo veritatis repudiandae vitae numquam repellat, facilis porro nostrum?</p>
-            <Button text="Login" class="btn-outline-info"/>
+          <h1 className='text-light'>Stock Prediction Portal</h1>
+          <p className='text-light lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deleniti commodi quasi quis. Sunt, et id libero nam voluptates quam! Distinctio possimus architecto accusantium aspernatur alias cupiditate laborum quia delectus iste explicabo veritatis repudiandae vitae numquam repellat, facilis porro nostrum?</p>
+          <Button text="Login" class="btn-outline-info" url="/login"/>
         </div>
-    </div>
-   </>
+      </div>
+    </>
   )
 }
 
